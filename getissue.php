59d4c516eb9e219eeebf265155709c86b13fd836
Jeeth Suresh <jeethsuresh@gmail.com>
2014-03-02 01:37:36 -0600

    Fixed CSS, Added Inline Image Support
    Image support in articles is now added - when submitting the article
    typing in “<img>” into the main article text tags that position for an
    image that’s also uploaded. During the upload it grabs the images from
    an array and shoves them into the article all pretty like.

--- getissue.php
+++ getissue.php
@@ -1,5 +1,5 @@
 <html>
-<link href='articlepage.css' rel='stylesheet' type='text/css' />
+
 
 <?php
 
@@ -16,13 +16,13 @@ $row_list = array();
 while($this_row = mysql_fetch_array($result)){
 	$row_list[$this_row['TableOrg']-1] = $this_row;
 }
-
+echo "<link href='issuepage.css' rel='stylesheet' type='text/css' />";
 echo '<script>
 function noFundRaisers(){
 	alert("We have no fundraisers going on!");
 }
 </script><head><ul id=menu class="center">
-<li><a href="http://greenobservermagazine.com/newindex.html" style="font-size: 42px">The Green Observer</a>
+<li><a href="http://greenobservermagazine.com/newindex.html" style="font-size: 38px">The Green Observer</a>
 <li><a href="http://greenobservermagazine.com/ArticlesAndImages/goissuefall2013.html">Current Issue</a>
 <li><a href="http://greenobservermagazine.com/previous%20issues.html">Past Issues</a>
 <li><a href="http://greenobservermagazine.com/SocialMedia.html">Social Media</a>
@@ -30,43 +30,43 @@ function noFundRaisers(){
 <li><a href="http://greenobservermagazine.com/about.html">About Us</a>
 <li><a href="javascript:noFundRaisers();">Donate!</a>
 </ul></head>';
-echo '<body><div class="container">';
+echo '<div id="wrap"><body><div class="container">';
 for($i = 0; $i < count($row_list); $i++){
 	$linkto = "http://thatkidfrommars.x10.mx/php/outwithphp.php?q=".$row_list[$i]['ID'];
 	if(substr($row_list[$i]['IssueOrganization'], 0, 9) == "spotlight"){
 		$imgstring = substr($row_list[$i]['IssueOrganization'], 10);
-		echo "
-		<div class='spotlight' style='display: block'>
-		<img class='spotlight' src={$imgstring} style='width: 500px; height: 400px; display: block; margin: 0 auto;'></img>
-		<a href={$linkto}><h1>{$row_list[$i]['Title']}</h1></a>
-		<p>{$row_list[$i]['Blurb']}</p>
+		echo "<div id='space' style='height: 56px;'></div>
+		<div class='spotlight'>
+			<img class='spotlightimage' src={$imgstring}></img>
+			<a href={$linkto}><h1>{$row_list[$i]['Title']}</h1></a>
+			<p>{$row_list[$i]['Blurb']}</p>
 		</div>";
 	}
 	else if(substr($row_list[$i]['IssueOrganization'], 0) == "feature"){
 		echo "
-		<div class='feature' style='float: left; width: 50%; height: 250px; text-align: center;'>
-		<a href={$linkto}><h3>{$row_list[$i]['Title']}</h3></a>
-		<p>{$row_list[$i]['Blurb']}</p>
+		<div class='featurenoimage'>
+			<a href={$linkto}><h3>{$row_list[$i]['Title']}</h3></a>
+			<p>{$row_list[$i]['Blurb']}</p>
 		</div>";
 	}
 	else if(substr($row_list[$i]['IssueOrganization'], 0, 8) == "feature,"){
 		$imgstring = substr($row_list[$i]['IssueOrganization'], 8);
 		echo "
-		<div class='feature' style='display: inline; width: 50%; height: 300px; float: left; overflow: auto; text-align: center'>
-		<img class='feature' src={$imgstring} style='width: 200px; height: 150px;display: block; margin: 0 auto;'></img>
-		<a href={$linkto}><h3>{$row_list[$i]['Title']}</h3></a>
-		<p>{$row_list[$i]['Blurb']}</p>
+		<div class='feature'>
+			<img class='imgfeature' src={$imgstring}></img>
+			<a href={$linkto}><h3>{$row_list[$i]['Title']}</h3></a>
+			<p>{$row_list[$i]['Blurb']}</p>
 		</div>";
 	}
 	else{
 		echo "
-		<div class='single' style='float:left; overflow: auto; display: inline; width: 25%; height: 300px; text-align: center'>
+		<div class='single'>
 		<a href={$linkto}><h3>{$row_list[$i]['Title']}</h3></a>
 		<p>{$row_list[$i]['Blurb']}</p>
 		</div>";
 	}
 }
-echo '</div></body>';	
+echo '</div></body></div>';	
 
 ?>
 </html>
--- outwithphp.php
+++ outwithphp.php
@@ -18,7 +18,13 @@ echo $title;
 
 $tags = $row['Tags'];
 $article = nl2br($row['Article']);
-$image = substr($row['Images'], 0, strpos($row['Images'], "&"));
+$imagesource = $row['Images'];
+$image = array();
+for($i = 0; $i < substr_count($imagesource, "&"); $i++){
+	$image[$i] = substr($imagesource, 0, strpos($imagesource, "&"));
+	$imagesource = substr($imagesource, strpos($imagesource, "&")+1);
+}
+$image[substr_count($imagesource, "&")+1] = substr($imagesource, strripos($imagesource, "uploads"), strripos($imagesource, "&"));
 $title = $row['Title'];
 
 echo '<script>
@@ -26,7 +32,7 @@ function noFundRaisers(){
 	alert("We have no fundraisers going on!");
 }
 </script><head><ul id=menu class="center">
-<li><a href="http://greenobservermagazine.com/newindex.html" style="font-size: 42px">The Green Observer</a>
+<li><a href="http://greenobservermagazine.com/newindex.html" style="font-size: 38px">The Green Observer</a>
 <li><a href="http://greenobservermagazine.com/ArticlesAndImages/goissuefall2013.html">Current Issue</a>
 <li><a href="http://greenobservermagazine.com/previous%20issues.html">Past Issues</a>
 <li><a href="http://greenobservermagazine.com/SocialMedia.html">Social Media</a>
@@ -35,17 +41,24 @@ function noFundRaisers(){
 <li><a href="javascript:noFundRaisers();">Donate!</a>
 </ul></head>';
 
-echo "<body><link href='articlepage.css' rel='stylesheet' type='text/css' />
+echo "</div>
+<div id='wrap'><body><link href='articlepage.css' rel='stylesheet' type='text/css' />
 <h1>{$title}</h1><br>";
 if($image != NULL){
-echo "<div id='wrapper' style='text-align: center'><img src={$image} style='width: 500px;' /></div><br>";
+	echo "<div id='wrapper' style='text-align: center'><img src={$image[0]} /></div><br>";
 }
+for($j = 1; $j <= count($image); $j++){
+	if($image[$j] != ""){
+		$article = preg_replace("/<img>/", "<div id='wrapper' style='text-align: center; padding: 15px'><img src={$image[$j]}></img></div>", $article, 1);
+	}
+}	
+
 echo "<article>{$article}</article>";
 if($row['MediaEmbedCode'] != NULL){
-echo "<div id='video'><center>{$row['MediaEmbedCode']}</center></div>";
+	echo "<div id='video'><center>{$row['MediaEmbedCode']}</center></div>";
 }
 echo "<p>Tags: {$tags}</p><br>
-<p>Author: {$row['Author']}	</p></body>";
+<p>Author: {$row['Author']}	</p></body></div>";
 
 ?>
 
